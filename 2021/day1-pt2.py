bc157891af4eebd359010bf50a568cea3853c40c
Daniel Olaya <daniel.olayam@gmail.com>
2022-01-03 21:12:17 -0600

    WIP:day 1 pt2

--- 2021/day1-pt2.py
+++ 2021/day1-pt2.py
@@ -13,26 +13,39 @@ test = [199,
 
 fp = open('2021/day1-pt2-input.txt')
 
-increased = 0
-decreased = 0
-w1 = 0 
-w2 = 0
-w3 = 0
 for index, reading in enumerate (fp):
     content = fp.readlines()
 
-for index, line in enumerate(content):
-    print(f'index:{index}, line{line}')
-    print(f'line: {line}, content[index+0]:{content[index+0]} ,\
-        content[index+1]: {content[index+1]}, \
-        content[index+2]: {content[index+2]}')
-    # for (i = 1; i == 3; i++)
-        # print (fp[])
-    if index == 5:
-        exit(0)
+window_sum, increased, decreased, prev, cur = 0, 0, 0, 0, 0
+# for index, line in enumerate(content):
+for index, line in enumerate(test):
+    window_sum = content[index+0] + content[index+1] + content[index+2]
 
+    print(f'\nindex:{index}, line{line}')
+    print(f'window_sum:{window_sum}, content[index+0]:{content[index+0]}, content[index+1]: {content[index+1]},content[index+2]: {content[index+2]}')
 
+    if index == 0:
+        prev = window_sum
+        continue
+
+    if prev < window_sum:
+        increased += 1
+    elif prev > window_sum:
+        decreased += 1
+    else:
+        print(f'????? prev:{prev}, window_sum:{window_sum}')
     
+    if index == (len(content) - 3):
+        print(f'end of the rope...')
+        break
+
+
+    prev = window_sum
+
+
+    # if index == 5:u
+    #     print(f'Index is 5')
+    #     exit(0)
 
 print(f'Decreased: {decreased}')
 print(f'Increased: {increased}')
